2b49958af4cc8be0371674ddc26f56abd02cb33f
Hector Ricardo Dominguez <hrdominguez89@gmail.com>
2023-12-01 16:39:45 -0300

    se envia endpoint de devolucion

--- src/Controller/Secure/ComputersController.php
+++ src/Controller/Secure/ComputersController.php
@@ -197,6 +197,62 @@ class ComputersController extends AbstractController
     }
 
     /**
+     * @Route("/return", name="return_computer", methods={"POST"})
+     */
+    public function returnComputer(Request $request, StatusComputerRepository $statusComputerRepository,RequestsComputersRepository $requestsComputersRepository, EntityManagerInterface $em): JsonResponse
+    {
+        if ($this->user->getRol()->getId() != Constants::ROLE_PROFESSOR) {
+
+            $body = $request->getContent();
+            $data = json_decode($body, true);
+
+            if (!isset($data['id'])) {
+                return $this->json(
+                    [
+                        'message' => 'Error al enviar los datos, se espera un id',
+                    ],
+                    Response::HTTP_BAD_REQUEST,
+                    ['Content-Type' => 'application/json']
+                );
+            }
+            
+            $request_computer_id = $requestsComputersRepository->find($data['id']);
+
+            if(!$request_computer_id){
+                return $this->json(
+                    [
+                        'message' => 'No se encontro la computadora con el id indicado.',
+                    ],
+                    Response::HTTP_NOT_FOUND,
+                    ['Content-Type' => 'application/json']
+                );
+            }
+
+            $status_computer_available = $statusComputerRepository->find(Constants::STATUS_COMPUTER_AVAILABLE);
+
+            $request_computer_id->getComputer()->setStatusComputer($status_computer_available);
+            $request_computer_id->setReturnetAt(new \DateTime());
+
+
+
+            $em->persist($request_computer_id);
+            $em->flush();
+
+            return $this->json(
+                ['message' => 'La computadora se devolvio correctamente'],
+                Response::HTTP_CREATED,
+                ['Content-Type' => 'application/json']
+            );
+        }
+
+        return $this->json(
+            ['message' => 'Su cuenta no tiene permisos para realizar esta operación'],
+            Response::HTTP_FORBIDDEN,
+            ['Content-Type' => 'application/json']
+        );
+    }
+
+    /**
      * @Route("/{computer_id}", name="computers_by_id", methods={"GET","PATCH"})
      */
     public function computersById($computer_id, ComputersRepository $computersRepository, StatusComputerRepository $statusComputerRepository, Request $request, EntityManagerInterface $em): JsonResponse
